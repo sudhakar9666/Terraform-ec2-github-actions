22149bcb5ae99292dbf6b21a75df0989040c6d87
sudhakar9666 <149573730+sudhakar9666@users.noreply.github.com>
2025-10-22 11:40:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
-variable "aws_region"{
-    type = string
-    default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region to deploy"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "AMI ID for EC2"
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2
 }
